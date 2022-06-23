c04fd1f07126f3e532527def26f8336896579726
NH-Rifat <rifatr729@gmail.com>
2022-06-23 08:26:21 +0600

    reducer and action declared

--- src/services/actions/CounterAction.jsx
+++ src/services/actions/CounterAction.jsx
@@ -1,3 +1,19 @@
-export const INCREMENT = 'INCREMENT';
-export const RESET = 'RESET';
-export const DECREMENT = 'DECREMENT';
+import { DECREMENT, INCREMENT, RESET } from '../constants/counterConstants';
+
+export const incrementCounter = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+
+export const resetCounter = () => {
+  return {
+    type: RESET,
+  };
+};
+
+export const decrementCounter = () => {
+  return {
+    type: DECREMENT,
+  };
+};
---
+++ src/services/reducers/CounterReducer.jsx
@@ -0,0 +1,28 @@
+import { DECREMENT, INCREMENT, RESET } from "../constants/CounterConstant";
+
+const initialState = { count: 0 };
+
+const CounterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case RESET:
+      return {
+        ...state,
+        count: 0,
+      };
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default CounterReducer;
